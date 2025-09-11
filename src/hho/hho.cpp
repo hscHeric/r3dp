4ec2e6069c645e92fdd94a692811ab3d93bbca4d
Heric da Silva Cruz <hericsilvaho@gmail.com>
2025-09-11 09:26:11 -0300

    usando a classe rng explicitamente

--- src/hho/hho.cpp
+++ src/hho/hho.cpp
@@ -1,21 +1,24 @@
 #include "hho.hpp"
 
 #include <algorithm>
+#include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <stdexcept>
 #include <vector>
 
 namespace r3dp::hho {
 
-  template <class Decoder, class RNG>
-  HHO<Decoder, RNG>::HHO( unsigned       population_size,
-                          unsigned       dimension,
-                          double         lower_bound,
-                          double         upper_bound,
-                          unsigned       max_iterations,
-                          unsigned       max_threads,
-                          const Decoder &ref_decoder,
-                          RNG           &ref_rng )
+  // The template parameter for HHO should only be 'Decoder'
+  template <class Decoder>
+  HHO<Decoder>::HHO( unsigned       population_size,
+                     unsigned       dimension,
+                     double         lower_bound,
+                     double         upper_bound,
+                     unsigned       max_iterations,
+                     unsigned       max_threads,
+                     const Decoder &ref_decoder,
+                     core::RNG     &ref_rng )
     : dimension( dimension )
     , population_size( population_size )
     , max_iterations( max_iterations )
@@ -39,19 +42,20 @@ namespace r3dp::hho {
     lower_bounds_vec.assign( dimension, lower_bound );
     upper_bounds_vec.assign( dimension, upper_bound );
 
-    best_fitness = std::numeric_limits<double>::infinity();  // o r3dp é de minimização
+    best_fitness = std::numeric_limits<double>::infinity();
     iteration    = 0;
   }
 
-  template <class Decoder, class RNG>
-  HHO<Decoder, RNG>::HHO( unsigned                   population_size,
-                          unsigned                   dimension,
-                          const std::vector<double> &lower_bounds,
-                          const std::vector<double> &upper_bounds,
-                          unsigned                   max_iterations,
-                          unsigned                   max_threads,
-                          const Decoder             &ref_decoder,
-                          RNG                       &ref_rng )
+  // The template parameter for HHO should only be 'Decoder'
+  template <class Decoder>
+  HHO<Decoder>::HHO( unsigned                   population_size,
+                     unsigned                   dimension,
+                     const std::vector<double> &lower_bounds,
+                     const std::vector<double> &upper_bounds,
+                     unsigned                   max_iterations,
+                     unsigned                   max_threads,
+                     const Decoder             &ref_decoder,
+                     core::RNG                 &ref_rng )
     : dimension( dimension )
     , population_size( population_size )
     , lower_bounds_vec( lower_bounds )
@@ -84,28 +88,28 @@ namespace r3dp::hho {
     iteration    = 0;
   }
 
-  template <class Decoder, class RNG>
-  const std::vector<double> &HHO<Decoder, RNG>::get_best_solution() const {
+  template <class Decoder>
+  const std::vector<double> &HHO<Decoder>::get_best_solution() const {
     return best_position;
   }
 
-  template <class Decoder, class RNG>
-  double HHO<Decoder, RNG>::get_best_fitness() const {
+  template <class Decoder>
+  double HHO<Decoder>::get_best_fitness() const {
     return best_fitness;
   }
 
-  template <class Decoder, class RNG>
-  const std::vector<double> &HHO<Decoder, RNG>::get_convergence_curve() const {
+  template <class Decoder>
+  const std::vector<double> &HHO<Decoder>::get_convergence_curve() const {
     return convergence_curve;
   }
 
-  template <class Decoder, class RNG>
-  unsigned HHO<Decoder, RNG>::get_iteration() const {
+  template <class Decoder>
+  unsigned HHO<Decoder>::get_iteration() const {
     return iteration;
   }
 
-  template <class Decoder, class RNG>
-  void HHO<Decoder, RNG>::reset() {
+  template <class Decoder>
+  void HHO<Decoder>::reset() {
     hawks.clear();
     best_position.clear();
     best_fitness = std::numeric_limits<double>::infinity();
@@ -113,33 +117,32 @@ namespace r3dp::hho {
     convergence_curve.clear();
   }
 
-  template <class Decoder, class RNG>
-  void HHO<Decoder, RNG>::initialize_hawks() {
+  template <class Decoder>
+  void HHO<Decoder>::initialize_hawks() {
     hawks.resize( population_size );
 
     // clang-format off
-    #pragma omp parallel for num_threads( max_threads ) default(none)  // clang-format on
+    #pragma omp parallel for num_threads(max_threads) default(none)
+    // clang-format on
     for ( size_t i = 0; i < population_size; ++i ) {
       hawks[i].resize( dimension );
       for ( unsigned j = 0; j < dimension; ++j ) {
-        hawks[i][j] = this->ref_rng.random_range( lower_bounds_vec[j], upper_bounds_vec[j] );
+        hawks[i][j] =
+          this->ref_rng.random_range<double>( lower_bounds_vec[j], upper_bounds_vec[j] );
       }
     }
   }
 
-  template <class Decoder, class RNG>
-  void HHO<Decoder, RNG>::step() {
+  template <class Decoder>
+  void HHO<Decoder>::step() {
     // Verifica o criterio de parada baseado no número máximo de iterações
-    // Aqui o algoritmo utiliza o número máximo de iterações como um parâmetro para ele
-    // logo acredito que não é possivel definir todo o algoritmo apenas definindo um outro criterio
-    // de parada como tempo limite (lembrar de testar depois colocando o max_iterations = tempo
-    // máximo em segundos) assim removendo a necessidade de definir um max_iterations para os testes
     if ( iteration >= max_iterations ) {
       return;  // não fazer nada pois já atingiu o limite;
     }
 
     // clang-format off
-    #pragma omp parallel for num_threads( max_threads ) default(none)  // clang-format on
+    #pragma omp parallel for num_threads(max_threads) default(none)
+    // clang-format on
     for ( size_t i = 0; i < population_size; ++i ) {
       for ( unsigned j = 0; j < dimension; ++j ) {
         hawks[i][j] = std::clamp( hawks[i][j], lower_bounds_vec[j], upper_bounds_vec[j] );
@@ -148,7 +151,8 @@ namespace r3dp::hho {
 
     std::vector<double> fitness_values( population_size, std::numeric_limits<double>::infinity() );
     // clang-format off
-    #pragma omp parallel for num_threads( max_threads )  // clang-format on
+    #pragma omp parallel for num_threads(max_threads) default(none)
+    // clang-format on
     for ( size_t i = 0; i < population_size; ++i ) {
       fitness_values[i] = this->ref_decoder.decode( hawks[i] );
     }
@@ -156,13 +160,13 @@ namespace r3dp::hho {
     double current_best_fitness = std::numeric_limits<double>::infinity();
     size_t current_best_index   = 0;
     for ( size_t i = 0; i < population_size; ++i ) {
-      if ( fitness_values[i] > current_best_fitness ) {
+      if ( fitness_values[i] < current_best_fitness ) {
         current_best_fitness = fitness_values[i];
         current_best_index   = i;
       }
     }
 
-    if ( current_best_fitness > best_fitness ) {
+    if ( current_best_fitness < best_fitness ) {
       best_fitness  = current_best_fitness;
       best_position = hawks[current_best_index];
     }
@@ -171,12 +175,22 @@ namespace r3dp::hho {
                                        static_cast<double>( iteration ) / max_iterations );
 
     // clang-format off
-    #pragma omp parallel for num_threads(max_threads)  // clang-format on
+    #pragma omp parallel for num_threads(max_threads) default(none)
+    // clang-format on
     for ( size_t i = 0; i < population_size; ++i ) {
       // energia de escape do coelho
-      double E0 = ( JUMP_STRENGTH_FACTOR * ref_rng.random_range( 0.0, 1.0 ) ) -
+      double E0 = ( JUMP_STRENGTH_FACTOR * ref_rng.random_range<double>( 0.0, 1.0 ) ) -
                   ESCAPE_ENERGY_BOUND_EXPLORATION;
       double escaping_energy = E1 * E0;
+
+      if ( std::abs( escaping_energy ) >= 1 ) {
+        // TODO: Fase de exploração
+        double q = ref_rng.random_range<double>( 0.0, 1.0 );
+
+      } else if ( std::abs( escaping_energy ) < 1 ) {
+        // TODO: Fase de transição
+      }
     }
   }
+
 }  // namespace r3dp::hho
